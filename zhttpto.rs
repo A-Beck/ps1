d30a50f3b7f530f835eeae0087589bf0ca7edea1
Andrew <adb2jb@virginia.edu>
2014-01-24 12:19:07 -0500

    zhttpto.rs changed to solve problem 3

--- zhttpto.rs
+++ zhttpto.rs
@@ -1,4 +1,3 @@
-//
 // zhttpto.rs
 //
 // Starting code for PS1
@@ -11,6 +10,7 @@
 // Weilin Xu and David Evans
 // Version 0.3
 
+
 #[feature(globs)];
 use std::io::*;
 use std::io::net::ip::{SocketAddr};
@@ -18,17 +18,22 @@ use std::{str};
 
 static IP: &'static str = "127.0.0.1";
 static PORT:        int = 4414;
+static mut count: int = 0;
 
 fn main() {
+    
     let addr = from_str::<SocketAddr>(format!("{:s}:{:d}", IP, PORT)).unwrap();
     let mut acceptor = net::tcp::TcpListener::bind(addr).listen();
     
     println(format!("Listening on [{:s}] ...", addr.to_str()));
     
+    
     for stream in acceptor.incoming() {
+
         // Spawn a task to handle the connection
         do spawn {
             let mut stream = stream;
+
             
             match stream {
                 Some(ref mut s) => {
@@ -40,23 +45,30 @@ fn main() {
                 None => ()
             }
             
+
             let mut buf = [0, ..500];
             stream.read(buf);
             let request_str = str::from_utf8(buf);
             println(format!("Received request :\n{:s}", request_str));
-            
-            let response: ~str = 
-                ~"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
+
+            unsafe {
+
+            count += 1;
+
+            let response: ~str = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n
                  <doctype !html><html><head><title>Hello, Rust!</title>
-                 <style>body { background-color: #111; color: #FFEEAA }
-                        h1 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red}
-                        h2 { font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green}
+                 <style>body \\{ background-color: \\#111; color: \\#FFEEAA \\}
+                        h1 \\{ font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm red\\}
+                        h2 \\{ font-size:2cm; text-align: center; color: black; text-shadow: 0 0 4mm green\\}
                  </style></head>
                  <body>
-                 <h1>Greetings, Krusty!</h1>
-                 </body></html>\r\n";
+                 <h1>Greetings, Krusty! Total hits = {:d} </h1>
+                 </body></html>\r\n", count);
+
             stream.write(response.as_bytes());
-            println!("Connection terminates.");
+            println!("Connection terminates");
+            }
         }
     }
-}
+}   
